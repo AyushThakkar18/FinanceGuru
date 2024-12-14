11f1abdb917f15007578b5a1e04d840fd4465d64
AyushThakkar18 <thakkarayush2003@gmail.com>
2024-12-14 02:15:42 -0600

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+// import "./App.css";
+import Dashboard from "./dashboard.jsx";
+import LoginPage from "./Loginpage.jsx";
+import Dash from "./Dash.jsx";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/Dash" element={<Dash/>}></Route>
+        </Routes>
+      </Router>
+           
+    </>
+  );
+}
+
+export default App;
